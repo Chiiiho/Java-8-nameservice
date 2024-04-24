9d614ee88a3dbf119b7029fb0cbee6f9014b6f35
chiiiho <163135461+Chiiiho@users.noreply.github.com>
2024-04-24 22:56:37 +0900

    大文字小文字を区別し、空白や該当しない文字のリクエストの場合はエラーを返す仕様に変更

--- src/main/java/com/example/nameservice/NameController.java
+++ src/main/java/com/example/nameservice/NameController.java
@@ -1,5 +1,8 @@
 package com.example.nameservice;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +19,16 @@ public class NameController {
     }
 
     @GetMapping("/names")
-    public List<Name> findByNames(@RequestParam String startsWith) {
-        return nameMapper.findByNameStartingWith(startsWith);
+    public ResponseEntity<List<Name>> findByNames(@RequestParam String startsWith) {
+        if (StringUtils.isEmpty(startsWith)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+        List<Name> names = nameMapper.findByNameStartingWith(startsWith);
+        if (names.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+
+        return ResponseEntity.ok(names);
     }
 }
--- src/main/java/com/example/nameservice/NameMapper.java
+++ src/main/java/com/example/nameservice/NameMapper.java
@@ -8,6 +8,6 @@ import java.util.List;
 @Mapper
 public interface NameMapper {
 
-    @Select("SELECT * FROM names WHERE name LIKE CONCAT(#{prefix}, '%')")
+    @Select("SELECT * FROM names WHERE name LIKE BINARY CONCAT(#{prefix}, '%')")
     List<Name> findByNameStartingWith(String prefix);
 }
