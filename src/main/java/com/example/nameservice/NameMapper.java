998e099a08c62363337b9f74b737335dd1749fc0
chiiiho <163135461+Chiiiho@users.noreply.github.com>
2024-04-29 16:38:33 +0900

    Service 処理を実装

--- src/main/java/com/example/nameservice/NameMapper.java
+++ src/main/java/com/example/nameservice/NameMapper.java
@@ -4,10 +4,14 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface NameMapper {
 
-    @Select("SELECT * FROM names WHERE name LIKE BINARY CONCAT(#{prefix}, '%')")
+    @Select("SELECT * FROM names WHERE name LIKE CONCAT(#{prefix}, '%')")
     List<Name> findByNameStartingWith(String prefix);
+
+    @Select("SELECT * FROM names WHERE id = #{id}")
+    Optional<Name> findById(int id);
 }
